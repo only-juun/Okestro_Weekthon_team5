cc07570460986278cbd1226600dd33c66bdfade7
kimkyeongmin <cheery7272@naver.com>
2023-09-19 10:52:04 +0900

    init : project init

---
+++ src/main/java/com/okestro/omok/OmokApplication.java
@@ -0,0 +1,13 @@
+package com.okestro.omok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/okestro/omok/OmokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okestro.omok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
