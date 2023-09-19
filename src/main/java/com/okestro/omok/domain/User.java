61caa8325df953dd4883eff527f65eba69a7d57f
kimkyeongmin <cheery7272@naver.com>
2023-09-19 11:37:51 +0900

    feat: User,Room,BaseTime Entity 설계 및 데이터베이스, JPA 설정

---
+++ src/main/java/com/okestro/omok/domain/User.java
@@ -0,0 +1,39 @@
+package com.okestro.omok.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTime{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(length = 30)
+    private String name;
+
+    @Column(length = 30)
+    private String email;
+
+    private String image;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @Builder
+    private User(Long id, String name, String email, String image, Room room) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.image = image;
+        this.room = room;
+    }
+}
