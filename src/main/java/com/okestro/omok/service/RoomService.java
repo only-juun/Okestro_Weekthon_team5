190e3d4bf3f4e111b8baa804cb4af888c574b53c
only-juun <nhj7334@icloud.com>
2023-09-19 15:09:40 +0900

    feat: 방 등록 기능
    - payload 패키지 생성
    - RoomSaveRequestDto 생성
    - RoomController, RoomService 수정

--- src/main/java/com/okestro/omok/service/RoomService.java
+++ src/main/java/com/okestro/omok/service/RoomService.java
@@ -1,6 +1,8 @@
 package com.okestro.omok.service;
 
 
+import com.okestro.omok.domain.Room;
+import com.okestro.omok.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,4 +11,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class RoomService {
+
+    private final RoomRepository roomRepository;
+
+    @Transactional
+    public void register(Room room) {
+        roomRepository.save(room);
+    }
 }
