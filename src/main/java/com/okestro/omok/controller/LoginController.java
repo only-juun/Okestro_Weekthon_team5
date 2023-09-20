a0b9df5452b9e1107c6e393c099f3e4b28dbeb19
only-juun <nhj7334@icloud.com>
2023-09-20 16:23:41 +0900

    feat: 구글 소셜 로그인 기능

---
+++ src/main/java/com/okestro/omok/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.okestro.omok.controller;
+
+import com.okestro.omok.service.LoginService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/login/oauth2")
+public class LoginController {
+    LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @GetMapping("/code/{registrationId}")
+    public void googleLogin(@RequestParam String code, @PathVariable String registrationId) {
+        loginService.socialLogin(code, registrationId);
+    }
+}
