962f1ffa2352714f9ab158519d9063c2d065f556
only-juun <nhj7334@icloud.com>
2023-09-21 11:26:15 +0900

    fix : Cors 문제 해결

---
+++ src/main/java/com/okestro/omok/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.okestro.omok.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(List.of(
+                            "http://localhost:3000",
+                            "http://localhost:5173",
+                            "http://localhost:8080"
+                    ));
+                    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    configuration.setAllowedHeaders(List.of("*"));
+                    return configuration;
+                }));
+
+        return http.build();
+    }
+
+}
